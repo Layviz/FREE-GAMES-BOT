4bc7476fe8646f3e299c1a7e59188cf3939b341a
Jörn Andreesen <jr.andreesen@gmail.com>
2025-12-13 03:54:32 +0100

    fixed automated check for games

--- free_games_bot.py
+++ free_games_bot.py
@@ -8,7 +8,9 @@ import shutil
 from time import sleep
 import datetime
 import pytz
-
+import asyncio
+from datetime import timedelta
+import traceback
 
 log_filename="free_games_bot.log"
 if(os.path.isfile(log_filename)):
@@ -40,15 +42,30 @@ client = discord.Client(intents=intents)
 async def on_ready():
     global CHANNEL
     logging.info("bot is ready")
+    logging.info(f"Current time: {datetime.datetime.now(pytz.timezone('Europe/Berlin'))}")
     CHANNEL = client.get_channel(CHANNEL_ID)
     if CHANNEL is None:
         logging.critical(f"failed to get channel with id {CHANNEL_ID}, check your config")
         SystemExit(2)
     logging.info(f"Using channel {CHANNEL.name} with id {CHANNEL.id} for updates")
     await check_for_games()
+    wait_for_loop_start_task = asyncio.create_task(wait_for_loop_start())
+    await wait_for_loop_start_task
+    
+
+async def wait_for_loop_start():
+    tz = pytz.timezone('Europe/Berlin')
+    now = datetime.datetime.now(tz)
+    target = now.replace(hour=19, minute=30, second=0, microsecond=0)
+    if target <= now:
+        target = target + timedelta(days=1)
+    wait_seconds = (target - now).total_seconds()
+    logging.info(f"sleeping for {wait_seconds} seconds until {target}")
+    await asyncio.sleep(wait_seconds)
+    await check_for_games.start()
 
 
-@tasks.loop(time=datetime.time(hour=19, minute=30, tzinfo=pytz.timezone("Europe/Berlin")) )
+@tasks.loop(hours=24)
 async def check_for_games():
     await client.wait_until_ready()
     try:
@@ -57,7 +74,7 @@ async def check_for_games():
         if new_games:
             logging.info(f"found {len(new_games)} new free games, sending messages")
             for title, link, platform in new_games:
-                if platform != "steam":
+                if platform == "steam":
                     message = f"{link}"
                     embed = discord.Embed(title=title, url=link, description=get_steam_game_description(link))
                     embed.set_author(name="Steam",url="https://store.steampowered.com")
@@ -67,10 +84,14 @@ async def check_for_games():
                         logging.info(f"sent message for {title}")
                     except Exception as e:
                         logging.error(f"failed to send message for {title}: {e}")
-                await sleep(1)  # avoid hitting rate limits
+                await asyncio.sleep(1)  # avoid hitting rate limits
         else:
             logging.info("no new free games found")
     except Exception as e:
         logging.error(f"error while checking for new free games: {e}")
+        for line in traceback.format_exception(e):
+            logging.error(line)
+
+
 
 client.run(TOKEN)
